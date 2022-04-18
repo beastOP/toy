c928abc598e50f7a6aa15609d46135db8fbcc8f6
Omkar Patil <omkarpatil290@gmail.com>
2022-04-18 11:49:30 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/beastOP/toy
+
+go 1.13
---
+++ repl/repl.go
@@ -0,0 +1,51 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/beastOP/toy/evaluator"
+	"github.com/beastOP/toy/lexer"
+	"github.com/beastOP/toy/object"
+	"github.com/beastOP/toy/parser"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some toy business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
